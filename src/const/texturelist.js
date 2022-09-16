6dde8dd1b12076c8049e946dc76643b02c3ef4fa
frankieali <frankieali4@gmail.com>
2022-09-16 11:00:45 -0400

    update to use .webp images

--- src/const/texturelist.js
+++ src/const/texturelist.js
@@ -2,214 +2,214 @@ export const TEXTURELIST = {
 	'cloudy': {
 		name: 'Clouds (Transparent)',
 		composite: 'destination-in',
-		source: './textures/cloudy.png',
-		source_bump: './textures/cloudy.alt.png'
+		source: './textures/cloudy.webp',
+		source_bump: './textures/cloudy.alt.webp'
 	},
 	'cloudy_2': {
 		name: 'Clouds',
 		composite: 'multiply',
-		source: './textures/cloudy.alt.png',
-		source_bump: './textures/cloudy.alt.png'
+		source: './textures/cloudy.alt.webp',
+		source_bump: './textures/cloudy.alt.webp'
 	},
 	'fire': {
 		name: 'Fire',
 		composite: 'multiply',
-		source: './textures/fire.png',
-		source_bump: './textures/fire.png',
+		source: './textures/fire.webp',
+		source_bump: './textures/fire.webp',
 		material: 'metal'
 	},
 	'marble': {
 		name: 'Marble',
 		composite: 'multiply',
-		source: './textures/marble.png',
+		source: './textures/marble.webp',
 		source_bump: '',
 		material: 'glass'
 	},
 	'water': {
 		name: 'Water',
 		composite: 'destination-in',
-		source: './textures/water.png',
-		source_bump: './textures/water.png',
+		source: './textures/water.webp',
+		source_bump: './textures/water.webp',
 		material: 'glass'
 	},
 	'ice': {
 		name: 'Ice',
 		composite: 'destination-in',
-		source: './textures/ice.png',
-		source_bump: './textures/ice.png',
+		source: './textures/ice.webp',
+		source_bump: './textures/ice.webp',
 		material: 'glass'
 	},
 	'paper': {
 		name: 'Paper',
 		composite: 'multiply',
-		source: './textures/paper.png',
-		source_bump: './textures/paper-bump.png',
+		source: './textures/paper.webp',
+		source_bump: './textures/paper-bump.webp',
 		material: 'wood'
 	},
 	'speckles': {
 		name: 'Speckles',
 		composite: 'multiply',
-		source: './textures/speckles.png',
-		source_bump: './textures/speckles.png',
+		source: './textures/speckles.webp',
+		source_bump: './textures/speckles.webp',
 		material: 'none'
 	},
 	'glitter': {
 		name: 'Glitter',
 		composite: 'multiply',
-		source: './textures/glitter.png',
-		source_bump: './textures/glitter-bump.png',
+		source: './textures/glitter.webp',
+		source_bump: './textures/glitter-bump.webp',
 		material: 'none'
 	},
 	'glitter_2': {
 		name: 'Glitter (Transparent)',
 		composite: 'destination-in',
-		source: './textures/glitter-alpha.png',
+		source: './textures/glitter-alpha.webp',
 		source_bump: '',
 		material: 'none'
 	},
 	'stars': {
 		name: 'Stars',
 		composite: 'multiply',
-		source: './textures/stars.png',
-		source_bump: './textures/stars.png',
+		source: './textures/stars.webp',
+		source_bump: './textures/stars.webp',
 		material: 'none'
 	},
 	'stainedglass': {
 		name: 'Stained Glass',
 		composite: 'multiply',
-		source: './textures/stainedglass.png',
-		source_bump: './textures/stainedglass-bump.png',
+		source: './textures/stainedglass.webp',
+		source_bump: './textures/stainedglass-bump.webp',
 		material: 'glass'
 	},
 	'wood': {
 		name: 'Wood',
 		composite: 'multiply',
-		source: './textures/wood.png',
-		source_bump: './textures/wood.png',
+		source: './textures/wood.webp',
+		source_bump: './textures/wood.webp',
 		material: 'wood'
 	},
 	'metal': {
 		name: 'Stainless Steel',
 		composite: 'multiply',
-		source: './textures/metal.png',
-		source_bump: './textures/metal-bump.png',
+		source: './textures/metal.webp',
+		source_bump: './textures/metal-bump.webp',
 		material: 'metal'
 	},
 	'skulls': {
 		name: 'Skulls',
 		composite: 'multiply',
-		source: './textures/skulls.png',
-		source_bump: './textures/skulls.png'
+		source: './textures/skulls.webp',
+		source_bump: './textures/skulls.webp'
 	},
 	'leopard': {
 		name: 'Leopard',
 		composite: 'multiply',
-		source: './textures/leopard.png',
-		source_bump: './textures/leopard.png',
+		source: './textures/leopard.webp',
+		source_bump: './textures/leopard.webp',
 		material: 'wood'
 	},
 	'tiger': {
 		name: 'Tiger',
 		composite: 'multiply',
-		source: './textures/tiger.png',
-		source_bump: './textures/tiger.png',
+		source: './textures/tiger.webp',
+		source_bump: './textures/tiger.webp',
 		material: 'wood'
 	},
 	'cheetah': {
 		name: 'Cheetah',
 		composite: 'multiply',
-		source: './textures/cheetah.png',
-		source_bump: './textures/cheetah.png',
+		source: './textures/cheetah.webp',
+		source_bump: './textures/cheetah.webp',
 		material: 'wood'
 	},
 	'dragon': {
 		name: 'Dragon',
 		composite: 'multiply',
-		source: './textures/dragon.png',
-		source_bump: './textures/dragon-bump.png',
+		source: './textures/dragon.webp',
+		source_bump: './textures/dragon-bump.webp',
 		material: 'none'
 	},
 	'lizard': {
 		name: 'Lizard',
 		composite: 'multiply',
-		source: './textures/lizard.png',
-		source_bump: './textures/lizard.png',
+		source: './textures/lizard.webp',
+		source_bump: './textures/lizard.webp',
 		material: 'none'
 	},
 	'bird': {
 		name: 'Bird',
 		composite: 'multiply',
-		source: './textures/feather.png',
-		source_bump: './textures/feather-bump.png',
+		source: './textures/feather.webp',
+		source_bump: './textures/feather-bump.webp',
 		material: 'wood'
 	},
 	'astral': {
 		name: 'Astral Sea',
 		composite: 'multiply',
-		source: './textures/astral.png',
-		source_bump: './textures/stars.png',
+		source: './textures/astral.webp',
+		source_bump: './textures/stars.webp',
 		material: 'none'
 	},
 	'acleaf': {
 		name: 'AC Leaf',
 		composite: 'multiply',
-		source: './textures/acleaf.png',
-		source_bump: './textures/acleaf.png',
+		source: './textures/acleaf.webp',
+		source_bump: './textures/acleaf.webp',
 		material: 'none'
 	},
 	'thecage': {
 		name: 'Nicholas Cage',
 		composite: 'multiply',
-		source: './textures/thecage.png',
+		source: './textures/thecage.webp',
 		source_bump: '',
 		material: 'metal'
 	},
 	'isabelle': {
 		name: 'Isabelle',
 		composite: 'source-over',
-		source: './textures/isabelle.png',
+		source: './textures/isabelle.webp',
 		source_bump: '',
 		material: 'none'
 	},
 	'bronze01': {
 		name: 'bronze01',
 		composite: 'difference',
-		source: './textures/bronze/01.png',
+		source: './textures/bronze/bronze01.webp',
 		source_bump: '',
 		material: 'metal'
 	},
 	'bronze02': {
 		name: 'bronze02',
 		composite: 'difference',
-		source: './textures/bronze/02.png',
+		source: './textures/bronze/bronze02.webp',
 		source_bump: '',
 		material: 'metal'
 	},
 	'bronze03': {
 		name: 'bronze03',
 		composite: 'difference',
-		source: './textures/bronze/03.png',
+		source: './textures/bronze/bronze03.webp',
 		source_bump: '',
 		material: 'metal'
 	},
 	'bronze03a': {
 		name: 'bronze03a',
 		composite: 'difference',
-		source: './textures/bronze/03a.png',
+		source: './textures/bronze/bronze03a.webp',
 		source_bump: '',
 		material: 'metal'
 	},
 	'bronze03b': {
 		name: 'bronze03b',
 		composite: 'difference',
-		source: './textures/bronze/03b.png',
+		source: './textures/bronze/bronze03b.webp',
 		source_bump: '',
 		material: 'metal'
 	},
 	'bronze04': {
 		name: 'bronze04',
 		composite: 'difference',
-		source: './textures/bronze/04.png',
+		source: './textures/bronze/bronze04.webp',
 		source_bump: '',
 		material: 'metal'
 	},
